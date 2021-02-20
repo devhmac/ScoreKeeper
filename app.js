9ebc81fddb211757a3c992a8b889189defcb9b25
Devin MacGillivray <devhmac@gmail.com>
2021-02-20 15:47:24 -0700

    logic complete

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ let roundNums = Number(rounds.value);
 
 
 p1Point.addEventListener('click', () => {
-  if (p1Score !== roundNums && p2Score !== roundNums) {
+  if (p1Score !== roundNums && p2Score !== roundNums) { //have to put the logic inside the event listener, this  will change what the targeted element does
     p1Score += 1
     p1Display.innerText = p1Score;
   }
-
+  if (p1Score === roundNums) {
+    p1Display.classList.add('winner');
+    p2Display.classList.add('loser');
+  }
 })
 
 
@@ -29,20 +32,29 @@ p2Point.addEventListener('click', () => {
     p2Score += 1;
     p2Display.innerText = p2Score;
   }
+  if (p2Score === roundNums) {
+    p2Display.classList.add('winner');
+    p1Display.classList.add('loser');
+  }
 })
 
-resetButton.addEventListener('click', () => {
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.innerText = p1Score;
-  p2Display.innerText = p2Score;
-})
+resetButton.addEventListener('click', reset)
 
 rounds.addEventListener('change', function () {
 
   roundNums = Number(this.value);
+  p1Display.classList.remove('winner', 'loser')
+  p2Display.classList.remove('winner', 'loser')
 
 })
 
+function reset() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.innerText = p1Score;
+  p2Display.innerText = p2Score;
+  p1Display.classList.remove('winner', 'loser')
+  p2Display.classList.remove('winner', 'loser')
+}
 
 

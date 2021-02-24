7bdbc2c34424946c415372cac91205ba0354495c
Devin MacGillivray <devhmac@gmail.com>
2021-02-24 11:24:35 -0700

    refactored reset function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//inputs
+
 const p1 = {
   score: 0,
   button: document.querySelector('#p1Point'),
@@ -11,10 +11,8 @@ const p2 = {
   display: document.querySelector('#p2Display')
 }
 
-
 const resetButton = document.querySelector('#reset');
 const rounds = document.querySelector('#rounds');
-//values
 
 let roundNums = Number(rounds.value);
 
@@ -31,11 +29,11 @@ p2.button.addEventListener('click', () => { // on p2 button press
 resetButton.addEventListener('click', reset)
 
 rounds.addEventListener('change', function () {
-
   roundNums = Number(this.value);
   p1.display.classList.remove('winner', 'loser')
   p2.display.classList.remove('winner', 'loser')
-
+  p1.button.disabled = false
+  p2.button.disabled = false
 })
 
 function updateScores(player, opponent) {
@@ -47,16 +45,18 @@ function updateScores(player, opponent) {
   if (player.score === roundNums) {
     player.display.classList.add('winner');
     opponent.display.classList.add('loser');
+    player.button.disabled = true
+    opponent.button.disabled = true
   }
 }
 
-function reset() {
-  p1.score = 0;
-  p2.score = 0;
-  p1.display.innerText = p1.score;
-  p2.display.innerText = p2.score;
-  p1.display.classList.remove('winner', 'loser')
-  p2.display.classList.remove('winner', 'loser')
+function reset() { // refactored to loop through each players properties, instead of targeting individually
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.innerText = p.score;
+    p.display.classList.remove('winner', 'loser');
+    p.button.disabled = false
+
+
+  }
 }
-
-

33c295f930ce665b27e1cc328c76ff0dc704244c
Devin MacGillivray <devhmac@gmail.com>
2021-02-24 11:05:13 -0700

    refactoring code to remove redundancy, added player based objects, and a score function with all logic included

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
 //inputs
-const p1Point = document.querySelector('#p1Point');
-const p2Point = document.querySelector('#p2Point');
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Point'),
+  display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Point'),
+  display: document.querySelector('#p2Display')
+}
+
+
 const resetButton = document.querySelector('#reset');
 const rounds = document.querySelector('#rounds');
 //values
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
 
-let p1Score = 0;
-let p2Score = 0;
 let roundNums = Number(rounds.value);
 
 
-
-
-
-p1Point.addEventListener('click', () => { //updateing player 1 score, and determining win con
-  if (p1Score !== roundNums && p2Score !== roundNums) { //have to put the logic inside the event listener, this  will change what the targeted element does
-    p1Score += 1
-    p1Display.innerText = p1Score;
-  }
-  if (p1Score === roundNums) {
-    p1Display.classList.add('winner');
-    p2Display.classList.add('loser');
-  }
+p1.button.addEventListener('click', () => {  //on p1 button press 
+  updateScores(p1, p2)
 })
-// p1Point & p2Point are super redundant, could probably find a way to incorperate into a function
 
-p2Point.addEventListener('click', () => {
-  if (p1Score !== roundNums && p2Score !== roundNums) {
-    p2Score += 1;
-    p2Display.innerText = p2Score;
-  }
-  if (p2Score === roundNums) {
-    p2Display.classList.add('winner');
-    p1Display.classList.add('loser');
-  }
+
+p2.button.addEventListener('click', () => { // on p2 button press
+  updateScores(p2, p1)
 })
 
 resetButton.addEventListener('click', reset)
@@ -43,18 +33,30 @@ resetButton.addEventListener('click', reset)
 rounds.addEventListener('change', function () {
 
   roundNums = Number(this.value);
-  p1Display.classList.remove('winner', 'loser')
-  p2Display.classList.remove('winner', 'loser')
+  p1.display.classList.remove('winner', 'loser')
+  p2.display.classList.remove('winner', 'loser')
 
 })
 
+function updateScores(player, opponent) {
+
+  if (player.score !== roundNums && opponent.score !== roundNums) { //have to put the logic inside the event listener, this  will change what the targeted element does
+    player.score += 1;
+    player.display.innerText = player.score;
+  }
+  if (player.score === roundNums) {
+    player.display.classList.add('winner');
+    opponent.display.classList.add('loser');
+  }
+}
+
 function reset() {
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.innerText = p1Score;
-  p2Display.innerText = p2Score;
-  p1Display.classList.remove('winner', 'loser')
-  p2Display.classList.remove('winner', 'loser')
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.innerText = p1.score;
+  p2.display.innerText = p2.score;
+  p1.display.classList.remove('winner', 'loser')
+  p2.display.classList.remove('winner', 'loser')
 }
 
 
